278b1a2c58998d5c4f23b3e4e60d746ce9d038c9
SpYDeR <levashov_vanya@mail.ru>
2021-07-22 14:01:26 +0300

    Add Firts H/W

---
+++ Level_1/TemperatureController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HW_1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemperatureController : ControllerBase
+    {
+        private readonly Templist _holder;
+
+        public TemperatureController(Templist holder)
+        {
+            _holder = holder;
+        }
+        [HttpGet]
+        public IEnumerable<TemperatureList> List(DateTime Date1, DateTime Date2)
+        {
+            List<TemperatureList> OutList = _holder.AddToList(Date1, Date2);
+            return Enumerable.Range(0, OutList.Count()).Select(index => new TemperatureList
+            {
+                Date = OutList[index].Date,
+                Temperature = OutList[index].Temperature
+            })
+            .ToArray();
+        }
+        [HttpPost]
+        public IActionResult Save([FromQuery] DateTime Date, float Temp)
+        {
+            _holder.Add(Date, Temp);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Change([FromQuery] DateTime Date, float Temp)
+        {
+            _holder.Update(Date, Temp);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult Delete(DateTime Date1, DateTime Date2)
+        {
+            _holder.Del(Date1, Date2);
+            return Ok();
+        }     
+    }
+}
---
+++ Level_1/TemperatureList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW_1
+{
+    public class TemperatureList
+    {
+        public DateTime Date { get; set; }
+        public float Temperature { get; set; }
+    }
+    public class Templist
+    {
+        List<TemperatureList> TList = new List<TemperatureList>();
+        public void Add(DateTime Date, float Temp)
+        {
+            TList.Add(new TemperatureList { Date = Date, Temperature = Temp });
+        }
+        public void Update(DateTime Date, float Temp)
+        {
+            if (TList.Exists(index => index.Date == Date))
+            {
+                TList[TList.FindIndex(index => index.Date == Date)].Temperature = Temp;
+            }
+        }
+        public void Del(DateTime Date1, DateTime Date2)
+        {
+            for (DateTime date = Date1; date < Date2; date = date.AddDays(1))
+            {
+                var itemToRemove = TList.SingleOrDefault(index => index.Date == date);
+                if (itemToRemove != null)
+                    TList.Remove(itemToRemove);
+            }
+        }
+        public List<TemperatureList> AddToList(DateTime Date1, DateTime Date2)
+        {
+            List<TemperatureList> Temperature = new List<TemperatureList>();
+            for (DateTime date = Date1; date < Date2; date = date.AddDays(1))
+            {
+                if (TList.Exists(index => index.Date == date))
+                {
+                    Temperature.Add(new TemperatureList { Date = TList[TList.FindIndex(index => index.Date == date)].Date, Temperature = TList[TList.FindIndex(index => index.Date == date)].Temperature });
+                }
+            }
+            return Temperature;
+        }
+    }
+}
+
