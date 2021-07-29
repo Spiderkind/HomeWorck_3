218aadd3ad205ffeff08900deee824d2aecd2ab7
SpYDeR <levashov_vanya@mail.ru>
2021-07-29 17:41:31 +0300

    small fix

--- Level_3/MetricsManager/MetricsAgent/Controllers/CpuMetricsController.cs
+++ Level_3/MetricsManager/MetricsAgent/Controllers/CpuMetricsController.cs
@@ -147,7 +147,7 @@ namespace MetricsAgent.Controllers
                             {
                                 Id = reader.GetInt32(0), // читаем данные полученные из базы данных
                                 Value = reader.GetInt32(1), // преобразуя к целочисленному типу
-                                Time = TimeSpan.FromTicks(2)
+                                Time = TimeSpan.FromSeconds(reader.GetInt32(1))
                             };
                             // увеличиваем значение счетчика
                             counter++;
